d1415a6b4465370f2c1c3e7bd3a7be855a6d4c6c
larrytech7 <larryakah@gmail.com>
2017-01-02 20:14:17 +0100

    Created incidence object that can be used to characterise an incidence.

---
+++ app/src/main/java/com/satra/traveler/models/Incident.java
@@ -0,0 +1,87 @@
+package com.satra.traveler.models;
+
+import com.orm.SugarRecord;
+import com.orm.dsl.Unique;
+
+/**
+ * Created by Larry Akah on 6/14/16.
+ */
+public class Incident extends SugarRecord {
+
+    @Unique
+    private String key;
+    private String matricule;
+    private String agency;
+    private float speed;
+    private double acc;
+    private double latitude;
+    private double longitude;
+    private long timestamp;
+
+    public Incident() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public double getAcc() {
+        return acc;
+    }
+
+    public void setAcc(double acc) {
+        this.acc = acc;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
--- app/src/main/java/com/satra/traveler/models/Issue.java
+++
@@ -1,9 +0,0 @@
-package com.satra.traveler.models;
-
-import com.orm.SugarRecord;
-
-/**
- * Created by Larry Akah on 6/14/16.
- */
-public class Issue extends SugarRecord {
-}
